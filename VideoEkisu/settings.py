e0abe3e582695b3a3c61ef7344b5ddf632a92a7b
Seungwon Kim <closer527@gmail.com>
2015-03-14 17:26:06 +0900

    디버그 모드 해제

--- VideoEkisu/settings.py
+++ VideoEkisu/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'qkh9omt_z_x^7tou(tgf&zaylp^==*cm^9iv2_yf)f3)$wmt!e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
